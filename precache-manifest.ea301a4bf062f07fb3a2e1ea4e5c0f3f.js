b88538ac48e9990f80529e310ea30807447012a6
emavgl <emavgl@gmail.com>
2019-01-22 15:19:48 +0100

    Updates

---
+++ precache-manifest.ea301a4bf062f07fb3a2e1ea4e5c0f3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coffees-counter/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b9415099f3badbcc773c",
+    "url": "/coffees-counter/static/js/runtime~main.b9415099.js"
+  },
+  {
+    "revision": "10ebed5d95a5cc03c752",
+    "url": "/coffees-counter/static/js/main.10ebed5d.chunk.js"
+  },
+  {
+    "revision": "48e875c1f5733597409d",
+    "url": "/coffees-counter/static/js/1.48e875c1.chunk.js"
+  },
+  {
+    "revision": "10ebed5d95a5cc03c752",
+    "url": "/coffees-counter/static/css/main.be7719d3.chunk.css"
+  },
+  {
+    "revision": "48e875c1f5733597409d",
+    "url": "/coffees-counter/static/css/1.93e9eb4f.chunk.css"
+  },
+  {
+    "revision": "b373e4490d49ed1433c44f3ad41e2749",
+    "url": "/coffees-counter/index.html"
+  }
+];
